9886a2c1cd054f6b3f87bb4b025687a6182e98b8
“Snappyhacker” <jimodhiambo515@gmail.com>
2024-07-06 11:12:43 +0300

    event lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Your code here
+function moveDodgerLeft() {
+    const dodger = document.getElementById("dodger");
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  }
+  
+  function moveDodgerRight() {
+    const dodger = document.getElementById("dodger");
+    let leftNumbers = dodger.style.left.replace("px", "");
+    let left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left + 1}px`;
+    }
+  }
+  
